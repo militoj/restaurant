8fd817ff4a62683efe51b93549db1f368b4c1d77
militoj <militoj@gmail.com>
2017-03-13 18:58:59 -0500

    initialized variables and added getters and setters

--- MenuItem.java
+++ MenuItem.java
@@ -4,4 +4,40 @@ package org.launchcode.java.demos.java4python.restaurant;
  * Created by johnmilito on 3/13/17.
  */
 public class MenuItem {
+    private double price;
+    private String description;
+    private String category;
+    private Boolean newItem;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Boolean getNewItem() {
+        return newItem;
+    }
+
+    public void setNewItem(Boolean newItem) {
+        this.newItem = newItem;
+    }
 }
