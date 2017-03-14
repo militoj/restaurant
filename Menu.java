e7293d124c7d56fc3b11e8100adc1004abc3b26a
militoj <militoj@gmail.com>
2017-03-13 20:17:28 -0500

    cant get tostring to work

--- Menu.java
+++ Menu.java
@@ -1,37 +1,62 @@
 package org.launchcode.java.demos.java4python.restaurant;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 /**
  * Created by johnmilito on 3/13/17.
  */
 
 public class Menu {
-    private Date dateUpdated;
-    private ArrayList<MenuItem> menu;
+    private LocalDateTime dateUpdated;
+    private static ArrayList<MenuItem> menu = new ArrayList<>();
 
-    public Date getDateUpdated() {
+    public LocalDateTime getDateUpdated() {
         return dateUpdated;
     }
 
     public void setDateUpdated() {
-        this.dateUpdated = new Date();
+        this.dateUpdated = dateUpdated.now();
     }
 
     public ArrayList<MenuItem> getMenu() {
         return menu;
     }
 
-    public void setMenu(ArrayList<MenuItem> menu) {
-        this.menu = menu;
-        setDateUpdated();
+    public void setMenu(MenuItem menu) {
+        this.menu.add(menu);
+        this.setDateUpdated();
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "dateUpdated=" + dateUpdated +
+                "menu=" + menu.toString() +
+                '}';
     }
 
     public static void main(String[] args) {
         Menu breakfast = new Menu();
 
-        breakfast.setMenu();
+        MenuItem orangeJuice = new MenuItem();
+        orangeJuice.setPrice(2.65);
+        orangeJuice.setDescription("Orange Juice");
+        orangeJuice.setCategory("Beverage");
+        orangeJuice.setNewItem(Boolean.FALSE);
+
+        MenuItem bacon = new MenuItem();
+        bacon.setPrice(5.43);
+        bacon.setDescription("Bacon");
+        bacon.setCategory("Side");
+        bacon.setNewItem(Boolean.TRUE);
+
+        breakfast.setMenu(orangeJuice);
+        breakfast.setMenu(bacon);
+
+        System.out.println(breakfast.getMenu().toString());
+        //System.out.println(menu.toString());
+
     }
 
 
