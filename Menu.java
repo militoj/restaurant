d50b524a8d00072959973b7c7d87864959f87810
militoj <militoj@gmail.com>
2017-03-13 19:25:16 -0500

    developed getters and setters, working on calling new menu in main()

--- Menu.java
+++ Menu.java
@@ -1,13 +1,38 @@
 package org.launchcode.java.demos.java4python.restaurant;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by johnmilito on 3/13/17.
  */
+
 public class Menu {
-    private String dateUpdated;
+    private Date dateUpdated;
     private ArrayList<MenuItem> menu;
 
-    
-}
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated() {
+        this.dateUpdated = new Date();
+    }
+
+    public ArrayList<MenuItem> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(ArrayList<MenuItem> menu) {
+        this.menu = menu;
+        setDateUpdated();
+    }
+
+    public static void main(String[] args) {
+        Menu breakfast = new Menu();
+
+        breakfast.setMenu();
+    }
+
+
+    }
