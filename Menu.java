d748f0c3eca35a853fcbb77f6f3c2da2f1fd8195
militoj <militoj@gmail.com>
2017-03-13 19:07:39 -0500

    initialized variables in Menu.java

--- Menu.java
+++ Menu.java
@@ -1,7 +1,13 @@
 package org.launchcode.java.demos.java4python.restaurant;
 
+import java.util.ArrayList;
+
 /**
  * Created by johnmilito on 3/13/17.
  */
 public class Menu {
+    private String dateUpdated;
+    private ArrayList<MenuItem> menu;
+
+    
 }
